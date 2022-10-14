a71992d6ebb9538c2fc75d53f65068747ab95e45
Luis Santiago <luisdavidsantiagosantana@gmail.com>
2022-10-14 00:15:19 -0400

    feat: crea endpoint para obtener la lista de archivos disponibles

--- src/v1/components/files/controller.js
+++ src/v1/components/files/controller.js
@@ -1,10 +1,25 @@
-import { getFormatedFiles } from './services.js'
+import { getFormatedFiles, getSecretFileList } from './services.js'
 import { ExternalAPIError } from '../../errors.js'
 
 export default {
   getData: async (req, res) => {
     try {
       const data = await getFormatedFiles()
+      res.status(200).send(data)
+    } catch (error) {
+      if (error instanceof ExternalAPIError) {
+        return res.status(error.status).send({
+          message: error.message
+        })
+      }
+
+      res.sendStatus(500)
+      throw error
+    }
+  },
+  getList: async (req, res) => {
+    try {
+      const data = await getSecretFileList()
       res.status(200).send(data)
     } catch (error) {
       if (error instanceof ExternalAPIError) {
--- src/v1/components/files/routes.js
+++ src/v1/components/files/routes.js
@@ -4,5 +4,6 @@ import controller from './controller.js'
 const router = Router()
 
 router.get('/data', controller.getData)
+router.get('/list', controller.getList)
 
 export default router

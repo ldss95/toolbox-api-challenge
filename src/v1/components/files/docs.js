0a2ac453bf83001467892ebbca1b575fa97ba133
Luis Santiago <luisdavidsantiagosantana@gmail.com>
2022-10-14 00:12:59 -0400

    docs: crea documentación para el endpoint de obtener archivos JSON

---
+++ src/v1/components/files/docs.js
@@ -0,0 +1,40 @@
+/**
+ * @swagger
+ * tags:
+ *  name: Files
+ */
+
+/**
+ * @swagger
+ * /files/data:
+ *  get:
+ *      tags: [Files]
+ *      summary: Obtiene la lista con todos los archivos en formato JSON
+ *      responses:
+ *          200:
+ *              description: OK
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: array
+ *                          items:
+ *                              type: object
+ *                              properties:
+ *                                  file:
+ *                                      type: string
+ *                                      example: test1.csv
+ *                                  lines:
+ *                                      type: array
+ *                                      items:
+ *                                          type: object
+ *                                          properties:
+ *                                              text:
+ *                                                  type: string
+ *                                                  example: OPMZitgaacAa
+ *                                              number:
+ *                                                  type: string
+ *                                                  example: 52
+ *                                              hex:
+ *                                                  type: string
+ *                                                  example: ec3a3a0b54fea1d2b40333a4a75b2c53
+ */
--- src/v1/swaggerJsDoc.js
+++ src/v1/swaggerJsDoc.js
@@ -1,17 +1,18 @@
 import swaggerJsDoc from 'swagger-jsdoc'
 
 export const specsV1 = swaggerJsDoc({
-	definition: {
-		openapi: '3.0.1',
-		info: {
-			title: 'ToolBox Challenge API',
-			version: '1.0.0'
-		},
-		servers: [
-			{
-				url: 'http://localhost:3000/v1'
-			}
-		]
-	},
-	apis: ['./src/v1/components/**/docs.ts']
+  definition: {
+    openapi: '3.0.1',
+    info: {
+      title: 'ToolBox Challenge API',
+      version: '1.0.0'
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000/v1',
+        description: 'Servidor de desarrollo'
+      }
+    ]
+  },
+  apis: ['./src/v1/components/**/docs.js']
 })
